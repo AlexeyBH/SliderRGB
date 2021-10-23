9d9c6bffcf1b2b5078a92b09953131ae14539d33
Alexey_BH <alexey_bh@mail.ru>
2021-10-23 22:11:24 +0300

    Final version

--- SliderRGB/ViewController.swift
+++ SliderRGB/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var colorField: UIView!
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func changeColor() {
+        guard let r = sliderRed,
+              let g = sliderGreen,
+              let b = sliderBlue
+        else {
+            print("Outlet connection error..")
+            return
+        }
+        colorField.backgroundColor = UIColor(
+            red: CGFloat(r.value),
+            green: CGFloat(g.value),
+            blue: CGFloat(b.value),
+            alpha: 1.0
+        )
 
+    }
+    
 
 }
 
